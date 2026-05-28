9ef4b475282e5b933e75024f279dcae2f602f81c
surjya12 <surjya.mangraj123@gmail.com>
2026-05-29 00:50:07 +0530

    Singleton pattern

--- Singleton/src/Example.java
+++ Singleton/src/Example.java
@@ -1,11 +1,63 @@
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException, CloneNotSupportedException {
          Samosa samosa= Samosa.getSamosa();
         System.out.println(samosa.hashCode());
+//
+//        Samosa samosa2= Samosa.getSamosa();
+//        System.out.println(samosa2.hashCode());
+//
+//        System.out.println(Jalebi.getJalebi().hashCode());
+//        System.out.println(Jalebi.getJalebi().hashCode());
 
-        Samosa samosa2= Samosa.getSamosa();
-        System.out.println(samosa2.hashCode());
+        /*
+         * There are three ways to break singleton pattern
+         *
+         *  1. Reflection API to break singleton pattern
+         * solution: 1. if object is there => throw exception from inside constructor.
+         *           2. use enum
+         *
+         * 2. Deserialization
+         * solution: implementing readResolve method
+         *
+         *
+         * 3. cloning
+         *
+         *
+         * */
+
+//      1.  Reflection API to break singleton pattern START
+//        Samosa s2=Samosa.getSamosa();
+//        System.out.println(s2.hashCode());
+//
+//        Constructor<Samosa> declaredConstructors = Samosa.class.getDeclaredConstructor();
+//        declaredConstructors.setAccessible(true);
+//        Samosa s1= declaredConstructors.newInstance();
+//        System.out.println(s1.hashCode());
+//        Reflection API to break singleton pattern END
+
+
+//        2. Deserialization method START
+//        Samosa s3=Samosa.getSamosa();
+//        ObjectOutputStream oos= new ObjectOutputStream(new FileOutputStream("abc.ob"));
+//        oos.writeObject(s3);
+//        System.out.println(s3.hashCode());
+//        System.out.println("serialization done.");
+//
+//        ObjectInputStream ois=new ObjectInputStream(new FileInputStream("abc.ob"));
+//        Samosa s4= (Samosa) ois.readObject();
+//        System.out.println(s4.hashCode());
+//        Deserialization method END
+
+       Samosa s1=(Samosa) Samosa.getSamosa().clone();
+        System.out.println(s1.hashCode());
+
     }
+
+
 }
 
 
--- Singleton/src/Samosa.java
+++ Singleton/src/Samosa.java
@@ -1,13 +1,33 @@
-public class Samosa {
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Samosa implements Serializable,Cloneable {
     private static Samosa samosa;
     private Samosa(){
+        if(samosa != null){
+            throw new RuntimeException("You are trying to break singleton pattern.");
+        }
     }
 
+    // Lazy way to creating single object
     public static  Samosa getSamosa(){
         if(samosa==null){
-           samosa = new Samosa();
+            synchronized (Samosa.class){
+                if(samosa==null) {
+                    samosa = new Samosa();
+                }
+            }
         }
 
+        return samosa;
+    }
+
+    public Object readResolve(){
+        return samosa;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
         return samosa;
     }
 }
@@ -17,3 +37,5 @@ public class Samosa {
 // 2. object create with the help of method
 
 // 3. create field to store object is private
+
+//note :- this is not thread safty to get thread safe use keyword synchronized
